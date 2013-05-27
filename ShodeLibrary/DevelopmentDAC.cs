03c784b61ae155068e71efb08ec83fe822a18543
Albert García <albertgg93@gmail.com>
2013-05-28 00:54:00 +0200

    Development DAC Added
    The DAC of the developments has been completed and now it is fully
    functional with CUD operations.

--- ShodeLibrary/DevelopmentDAC.cs
+++ ShodeLibrary/DevelopmentDAC.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace ShodeLibrary
 {
@@ -13,6 +16,7 @@ namespace ShodeLibrary
         public DevelopmentDAC()
         {
             // Gets the string connection from a unique location
+            connection = ConfigurationManager.ConnectionStrings["ShodeDDBB"].ToString();
         }
 
         /* ****************************************************************** */
@@ -20,19 +24,55 @@ namespace ShodeLibrary
         /* ****************************************************************** */
         public string insert(DevelopmentBE development)
         {
-            string code = "";
-            // Do stuff here
-            return code;
+            string result = "User has been succesfully created!";
+            SqlConnection c = new SqlConnection(connection);
+            c.Open();
+
+            try
+            {
+                SqlCommand com = new SqlCommand("INSERT INTO contributions (project, usr, date, amount) " +
+                    "VALUES ('" + development.Project.Code + "','" + development.User.Email +
+                    "','" + development.Date.ToString("dd/mm/yyyy") + "'," + development.Ups.ToString() + ")", c);
+
+                com.ExecuteNonQuery();
+            }
+            catch (SqlException ex)
+            {
+                if (ex.Message.Contains("PRIMARY KEY"))
+                    result = "ERROR: A development has already been made today for that project.";
+            }
+            finally
+            {
+                c.Close();
+            }
+
+            return result;
         }
 
         public void update(DevelopmentBE development)
         {
-            // Do stuff here
+            SqlConnection c = new SqlConnection(connection);
+            c.Open();
+
+            SqlCommand com = new SqlCommand("UPDATE contributions " +
+                "SET project='" + development.Project.Code + "', usr='" + development.User.Email +
+                "', date='" + development.Date.ToString("dd/mm/yyyy") + "', amount=" + development.Ups.ToString(), c);
+
+            com.ExecuteNonQuery();
+            c.Close();
         }
         
         public void delete(DevelopmentBE development)
         {
-            // Do stuff here
+            SqlConnection c = new SqlConnection(connection);
+            c.Open();
+
+            SqlCommand com = new SqlCommand("DELETE FROM contributions WHERE " +
+                "project='" + development.Project.Code + "' AND usr='" + development.User.Email +
+                "' AND date='" + development.Date.ToString("dd/mm/yyyy") + "'", c);
+
+            com.ExecuteNonQuery();
+            c.Close();
         }
 
         /* ****************************************************************** */
